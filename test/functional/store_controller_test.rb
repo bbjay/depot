930e8fb6543968e3c3a8b1a994d9916a090dd6b7
bbjay <bbjay@here.local>
2013-04-02 13:50:10 +0200

    functional test extended

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,7 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select 'body .entry', 2 # see fixtures
   end
 
 end
