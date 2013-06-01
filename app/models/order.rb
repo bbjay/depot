9e87faed1b8f8768546e43fdf64a015464433c2c
bbjay <bbjay@here.local>
2013-06-02 00:54:17 +0200

    added checkout logic

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,15 @@
 class Order < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
   attr_accessible :address, :email, :name, :pay_type
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  def add_line_items_from_cart(cart)
+  	cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+	end
+  end
 end
